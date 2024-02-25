40fd598771c1268de5b294d8900bf711058ff3f6
tsano <69151078+tgtaku@users.noreply.github.com>
2024-02-25 17:40:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を使って値とキーを出力しよう</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 配列に値を代入する
+        $item_info = ['name'=>'onion','price'=>200,'weight'=>160];
+
+        // 配列の値を出力する
+        print_r($item_info);
+
+        ?>
+    </p>
+</body>
+
+</html>
